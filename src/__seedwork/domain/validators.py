36e7f0b4ae2b2edfd3d0af4172955c2b4a90103f
Iago Neres <iagoneresb@gmail.com>
2022-05-25 01:22:57 +0000

    feat: :sparkles: add entity validators

---
+++ src/__seedwork/domain/validators.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from typing import Any
+
+from __seedwork.domain.exceptions import ValidationException
+
+
+@dataclass(frozen=True, slots=True)
+class ValidatorRules:
+    value: Any
+    attribute: str
+
+    @staticmethod
+    def values(value: Any, attribute: str):
+        return ValidatorRules(value, attribute)
+
+    def required(self) -> 'ValidatorRules':
+        if self.value is None or self.value == "":
+            raise ValidationException(f'The "{self.attribute}" is required.')
+        return self
+
+    def string(self) -> 'ValidatorRules':
+        if not isinstance(self.value, str):
+            raise ValidationException(
+                f'The "{self.attribute}" must be a string.')
+        return self
+
+    def max_length(self, max_length: int) -> 'ValidatorRules':
+        if len(self.value) > max_length:
+            raise ValidationException(
+                f'The "{self.attribute}" mus be less than {max_length} characters.')
+        return self
+
+    def boolean(self) -> 'ValidatorRules':
+        if self.value is not True and self.value is not False:
+            raise ValidationException(
+                f'The "{self.attribute}" must be a boolean')
+        return self
